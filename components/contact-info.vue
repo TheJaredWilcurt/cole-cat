6b6126de4a349661f8f4061bd88f90a0254cb5aa
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2019-05-26 21:51:09 -0400

    bio

--- components/contact-info.vue
+++ components/contact-info.vue
@@ -9,6 +9,14 @@
       </div>
 
       <div class="col-12 col-md-6">
+        <div class="mb-3">
+          <strong>Bio:</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+          esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+          non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </div>
         <ul>
           <li>
             <strong>E-Mail:</strong>
