342cc87fc02772e8a421687c933cba6f13ab3770
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2019-05-27 09:55:31 -0400

    Style Swapper

--- components/style-swapper.vue
+++ components/style-swapper.vue
@@ -1,52 +1,55 @@
 <template>
   <div>
     <link
-      :href="'https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/' + selected + '/bootstrap.min.css'"
+      :href="'https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/' + selected.toLowerCase() + '/bootstrap.min.css'"
       rel="stylesheet"
     />
     <div class="form-group">
-      <select
-        v-model="selected"
-        class="custom-select"
-      >
-        <option
-          v-for="(style, styleIndex) in styles"
-          v-text="style"
-          :key="'style' + styleIndex"
-        ></option>
-      </select>
+      <label>
+        Theme:&nbsp;
+        <select
+          v-model="selected"
+          class="custom-select"
+        >
+          <option
+            v-for="(style, styleIndex) in styles"
+            v-text="style"
+            :key="'style' + styleIndex"
+          ></option>
+        </select>
+      </label>
     </div>
   </div>
 </template>
 
 <script>
 module.exports = {
-  name: 'style-swapper',
+  name: 'theme-swapper',
   data: function () {
     return {
-      selected: 'minty',
+      selected: 'Minty',
       styles: [
-        'cerulean',
-        'cosmo',
-        'cyborg',
-        'darkly',
-        'flatly',
-        'journal',
-        'litera',
-        'lumen',
-        'lux',
-        'materia',
-        'minty',
-        'pulse',
-        'sandstone',
-        'simplex',
-        'sketchy',
-        'slate',
-        'solar',
-        'spacelab',
-        'superhero',
-        'united',
-        'yeti'
+        'Cerulean',
+        'Cosmo',
+        'Cyborg',
+        'Darkly',
+        'Flatly',
+        'Journal',
+        'Litera',
+        'Lumen',
+        'Lux',
+        'Materia',
+        'Minty',
+        'Pulse',
+        'Sandstone',
+        'Simplex',
+        'Sketchy',
+        'Slate',
+        'Solar',
+        'Spacelab',
+        'Superhero',
+        'United',
+        'Yeti'
       ]
     };
   }
