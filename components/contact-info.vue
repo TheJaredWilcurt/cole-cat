79fa196cfaa5903d34296dcba6f8836b5b914314
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2019-06-30 19:14:57 -0400

    move contact to markdown

--- components/contact-info.vue
+++ components/contact-info.vue
@@ -3,29 +3,7 @@
     <h1 class="mt-5 mb-5">Contact</h1>
     <div class="row">
       <div class="col-12">
-        <ul>
-          <li>{{ location }}</li>
-          <li>
-            <strong>E-Mail:</strong>
-            <a :href="'mailto:' + email">{{ email }}</a>
-          </li>
-          <li>
-            <strong>Phone:</strong>
-            <a :href="'tel:' + phone">{{ phone }}</a>
-          </li>
-          <li
-            v-for="(link, linkIndex) in links"
-            :key="'link' + linkIndex"
-          >
-            <a
-              v-if="link.url"
-              v-text="link.name"
-              :href="link.url"
-              :target="link.external ? '_blank' : '_self'"
-            ></a>
-            <span v-else>{{ link.name }}</span>
-          </li>
-        </ul>
+        <markdown-content file="content/contact.md"></markdown-content>
       </div>
     </div>
   </div>
@@ -34,29 +12,8 @@
 <script>
 module.exports = {
   name: 'contact-info',
-  data: function () {
-    return {
-      location: 'Portland, OR',
-      email: 'ColesHeartArt@gmail.com',
-      phone: '1-317-874-5385',
-      links: [
-        {
-          name: 'Facebook',
-          url: 'https://www.facebook.com/hope.anne.jones',
-          external: true
-        },
-        {
-          name: 'Instagram',
-          url: 'https://www.instagram.com/explore/tags/colesheartart/',
-          external: true
-        },
-        {
-          name: 'Twitter',
-          url: 'https://twitter.com/search?q=ColesHeartArt',
-          external: true
-        }
-      ]
-    };
+  components: {
+    'markdown-content': httpVueLoader('components/markdown-content.vue')
   }
 };
 </script>
